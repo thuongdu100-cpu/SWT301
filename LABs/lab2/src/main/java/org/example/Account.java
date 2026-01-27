2d414b6bdb4505ea4aacb4aaabfb7d73173043a7
thuongdu100-cpu <thuongdu100@gmail.com>
2026-01-27 16:27:37 +0700

    Add files via upload

---
+++ LABs/lab2/src/main/java/org/example/Account.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Account {
+    private String username;
+    private String password;
+    private String email;
+
+    public Account(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
